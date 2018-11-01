55b09807f7f3b8b7466472c65cd0a6cf7d18bbae
Kutepov <skutepov@ozon.ru>
2018-11-01 13:07:39 +0300

    async execution

--- app/async_exec_test.go
+++ app/async_exec_test.go
@@ -9,44 +9,43 @@ import (
 )
 
 func TestAsyncExec(t *testing.T) {
-	j := int64(0)
+	j := int64(0) // execution counter
 
-	taskCount := 100
+	taskCount := 100 // all tasks number
 	maxGoroutines := 14
 
 	concurrentGoroutines := make(chan struct{}, maxGoroutines)
 
+	// fill channel's buffer
 	for i := 0; i < maxGoroutines; i++ {
 		concurrentGoroutines <- struct{}{}
 	}
 
-	// done := make(chan bool)
+	done := make(chan bool)
+	waitForAllJobs := make(chan bool)
 
-	/*	waitForAllJobs := make(chan bool)
+	// goroutine starter:
+	go func() {
+		for i := 0; i < taskCount; i++ {
+			<-done // wait for some DONE than start new goroutine
+			concurrentGoroutines <- struct{}{}
+		}
+		waitForAllJobs <- true // waiting for all goroutines done
+	}()
 
-		go func() {
-
-			waitForAllJobs <- true
-		}()
-	*/
 	for i := 0; i < taskCount; i++ {
 		<-concurrentGoroutines
 		go func() {
 			exec(&j)
-			// done <- true
+			done <- true
 		}()
 	}
 
-	// for i := 0; i < taskCount; i++ {
-	// 	<-done
-	// 	concurrentGoroutines <- struct{}{}
-	// }
-
-	// <-waitForAllJobs
+	<-waitForAllJobs
 }
 
 func exec(j *int64) {
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(time.Duration(atomic.LoadInt64(j)) * time.Millisecond)
 	atomic.AddInt64(j, 1)
-	fmt.Println(runtime.NumGoroutine())
+	fmt.Printf("%d:%d\n", *j, runtime.NumGoroutine())
 }
