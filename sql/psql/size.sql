712e70a66d527a76a2624637f22421711f96fa3c
Kutepov <skutepov@ozon.ru>
2020-06-11 01:00:04 +0300

    sql

---
+++ sql/psql/size.sql
@@ -0,0 +1,5 @@
+SELECT nspname || '.' || relname AS "relation",
+       pg_size_pretty(pg_relation_size(C.oid)) AS "size"
+FROM pg_class C
+         LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+WHERE nspname NOT IN ('pg_catalog', 'information_schema') order by pg_relation_size(C.oid) desc;
