aa77524362b07dfb17b8af215dd657fd6e306f74
Kutepov <skutepov@ozon.ru>
2020-08-14 15:00:00 +0300

    indexes

--- sql/psql/index.sql
+++ sql/psql/index.sql
@@ -1,2 +1,11 @@
 -- неиспользуемые индексы
 
+SELECT * FROM pg_stat_all_indexes
+WHERE schemaname = 'public' AND relname = 'product_search';
+
+-- дубли
+
+SELECT array_agg(indexname) AS indexes, replace(indexdef, indexname, '') AS defn
+FROM pg_indexes
+GROUP BY defn
+HAVING count(*) > 1;
