d291f462dcf09e63ca581c5f62ec001da5d3e237
Sergey Kutepov <skutepov@ozon.ru>
2022-06-17 14:55:35 +0300

    machine

---
+++ machine/machine_test.go
@@ -0,0 +1,20 @@
+package machine
+
+import (
+	"testing"
+	"time"
+
+	"machine"
+)
+
+func TestArm(t *testing.T) {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 500)
+
+		led.High()
+		time.Sleep(time.Millisecond * 500)
+	}
+}
